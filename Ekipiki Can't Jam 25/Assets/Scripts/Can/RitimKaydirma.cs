e12c541a1c2e419cb3cbb4ab94cdeeff9b74fcd6
Canka76 <karakoccan04@gmail.com>
2025-05-23 17:36:05 +0300

    bismillah

---
+++ Ekipiki Can't Jam 25/Assets/Scripts/Can/RitimKaydirma.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class RitimKaydirma : MonoBehaviour
+{
+    public GameObject BaseGameObject;
+    public GameObject slider;
+    public GameObject GoalGameObject;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
