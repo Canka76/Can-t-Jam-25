3030437a9e906a02c00cd00917b11f8f453222d8
Canka76 <karakoccan04@gmail.com>
2025-05-24 16:19:08 +0300

    Calısıyor suan

---
+++ Ekipiki Can't Jam 25/Assets/Scripts/Can/QuickTimeEvent.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using TMPro;
+
+public class QuickTimeEvent : MonoBehaviour
+{
+    public TextMeshProUGUI qteText; // Assign in Inspector
+    public float timeLimit = 3f;
+
+    private KeyCode[] possibleKeys = { KeyCode.W, KeyCode.A, KeyCode.S, KeyCode.D };
+    private KeyCode currentKey;
+    private bool inputReceived = false;
+
+    void Start()
+    {
+        StartCoroutine(StartQTE());
+    }
+
+    IEnumerator StartQTE()
+    {
+        // Pick a random key
+        currentKey = possibleKeys[Random.Range(0, possibleKeys.Length)];
+        qteText.text = $"PRESS: {currentKey}";
+        inputReceived = false;
+
+        float timer = 0f;
+        while (timer < timeLimit && !inputReceived)
+        {
+            if (Input.GetKeyDown(currentKey))
+            {
+                inputReceived = true;
+                qteText.text = "Success!";
+                break;
+            }
+            else if (AnyWrongKeyPressed())
+            {
+                inputReceived = true;
+                qteText.text = "Wrong Key!";
+                break;
+            }
+
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        if (!inputReceived)
+        {
+            qteText.text = "Too Late!";
+        }
+
+        // Wait a bit before starting again
+        yield return new WaitForSeconds(2f);
+        StartCoroutine(StartQTE());
+    }
+
+    bool AnyWrongKeyPressed()
+    {
+        foreach (KeyCode key in possibleKeys)
+        {
+            if (key != currentKey && Input.GetKeyDown(key))
+                return true;
+        }
+        return false;
+    }
+}
